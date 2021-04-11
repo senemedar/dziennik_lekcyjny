fd0883fa5df5267b720b794e9adffce9daaa7dfc
Michał Pielacha <mr.cheesewick@gmail.com>
2021-04-11 10:23:41 +0200

    database setup for local hosting

--- src/main/java/teamfp/dziennik/controller/TeacherController.java
+++ src/main/java/teamfp/dziennik/controller/TeacherController.java
@@ -1,8 +1,11 @@
 package teamfp.dziennik.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class TeacherController {
