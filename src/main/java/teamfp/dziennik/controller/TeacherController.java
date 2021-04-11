62b9a48c4b966f67d37ec60ded28b8bcd1f2f9ba
Michał Pielacha <mr.cheesewick@gmail.com>
2021-04-11 10:56:52 +0200

    database setup for local hosting

--- src/main/java/teamfp/dziennik/controller/TeacherController.java
+++ src/main/java/teamfp/dziennik/controller/TeacherController.java
@@ -1,16 +1,43 @@
 package teamfp.dziennik.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+import teamfp.dziennik.model.Teacher;
+import teamfp.dziennik.repository.TeacherRepository;
+import teamfp.dziennik.service.TeacherService;
+
+import java.util.List;
 
 @Controller
 public class TeacherController {
-    @RequestMapping(value = {"/teacherRegistration"}, method = RequestMethod.GET)
+	private final TeacherService teacherService;
+
+	@Autowired
+	TeacherRepository teacherRepository;
+
+	public TeacherController(TeacherService teacherService) {
+		this.teacherService = teacherService;
+	}
+
+	@GetMapping(value = {"/teacherRegistration"})
     public String index() {
-        return "static/teacherRegistration";
+        return "teachers/teacherRegistration";
     }
+
+    @GetMapping(value = "/teachers")
+	public String getTeachersList(Model model) {
+		List<Teacher> teacherList = teacherRepository.findAll();
+		model.addAttribute("teacher", teacherList);
+		return "teachers/teachersList";
+	}
+
+    @PostMapping(value = {"/addTeacher"})
+	public RedirectView postSaveTeacher(@ModelAttribute Teacher newTeacher) {
+    	teacherRepository.save(newTeacher);
+
+    	return new RedirectView("/teachers");
+	}
 }
